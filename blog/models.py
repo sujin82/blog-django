93fb3e1fc96540cd349141c3a0b716a8b660ae68
sujin82 <ryusujin82@gmail.com>
2025-07-04 18:45:03 +0900

    2단계 완료

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     CATEGORY_CHOICES = [
         ('travel', '여행'),
         ('food', '맛집'),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from .forms import PostForm
 
@@ -19,6 +19,8 @@ post_list = ListView.as_view(
 # 블로그 게시글 상세
 post_detail = DetailView.as_view(
     model=Post,
+    template_name="blog/post_detail.html",
+    context_object_name="post"
 )
 
 
@@ -29,26 +31,42 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     template_name="blog/post_write.html"
     success_url="/blog/"
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 post_write = PostCreateView.as_view()
 
 
 
 # 블로그 글 수정하기
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model=Post
     form_class=PostForm
     template_name="blog/post_edit.html"
-    success_url="/blog/" # 동적으로 변경(수정후에는 상세가 보이게)
+    success_url="/blog/" # ★ 동적으로 변경(수정후에는 상세가 보이게)
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
 
 post_edit = PostUpdateView.as_view()
 
 
 
-# 블로그 글 삭제 :: 테스트를 위해 post_confirm_delete.html 페이지로 생성했지만 js의 confirm()이나 모달 팝업으로 대체할 예정
-post_delete = DeleteView.as_view(
-    model=Post,
-    success_url="/blog/",
-)
+# 블로그 글 삭제 :: ★ 테스트를 위해 post_confirm_delete.html 페이지로 생성했지만 js의 confirm()이나 모달 팝업으로 대체할 예정
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name="blog/post_detail.html"
+    success_url="/blog/"
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+post_delete = PostDeleteView.as_view()
+
+
 
 
 # 게시글 검색 기능(FBV) : 타이틀검색(검색 필드 폼 활용), 카테고리(탭 디자인)
