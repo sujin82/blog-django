9ac7d579d141729553c82ac523b6b8d8364ace34
sujin82 <ryusujin82@gmail.com>
2025-07-06 16:13:17 +0900

    프로필 수정 추가 중

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegisterForm, ProfileForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import authenticate, login
 from django.db import transaction
 from django.contrib import messages
+from django.views.generic import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Profile
 
-def register(request):
+
+def register(request):  # CBV로 변경 예정
     if request.method == "POST":
         user_form = RegisterForm(request.POST)
         profile_form = ProfileForm(request.POST, request.FILES)
@@ -48,3 +52,19 @@ login_view = LoginView.as_view(
 )
 
 logout_view = LogoutView.as_view(next_page='/')
+
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = "accounts/profile_edit.html"
+    context_object_name = "profile"
+    success_url = ("/") # 확인
+
+    def get_object(self):
+        return get_object_or_404(Profile, user=self.request.user)
+    
+    def form_valid(self, form):
+        messages.success(self.request, '프로필이 수정되었습니다.')
+        return super().form_valid(form)
